6286909acc6ba92218a6ffc3506cd018958b4aa5
Theo <theo.tinti@epita.fr>
2022-07-01 21:49:02 +0200

    ok

--- montreal.py
+++ montreal.py
@@ -5,22 +5,22 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 mainDistricts = ["L'Île-Bizard–Sainte-Geneviève",
-                 "Pierrefonds-Roxboro", #
-                 "Saint-Laurent", #
-                 "LaSalle", #
+                 "Pierrefonds-Roxboro",
+                 "Saint-Laurent",
+                 "LaSalle",
                  "Verdun",
-                 "Le Sud-Ouest", #
-                 "Côte-des-Neiges–Notre-Dame-de-Grâce", #
+                 "Le Sud-Ouest",
+                 "Côte-des-Neiges–Notre-Dame-de-Grâce", 
                  "Outremont",
-                 "Ville-Marie", #
-                 "Le Plateau-Mont-Royal", #
-                 "Rosemont—La Petite-Patrie", #
-                 "Villeray-Saint-Michel-Parc-Extension", #
-                 "Ahuntsic-Cartierville", #
+                 "Ville-Marie",
+                 "Le Plateau-Mont-Royal",
+                 "Rosemont—La Petite-Patrie",
+                 "Villeray-Saint-Michel-Parc-Extension",
+                 "Ahuntsic-Cartierville",
                  "Montreal-Nord",
-                 "Saint-Leonard", #
-                 "Mercier–Hochelaga-Maisonneuve", #
-                 "Anjou"] #
+                 "Saint-Leonard",
+                 "Mercier–Hochelaga-Maisonneuve",
+                 "Anjou"]
 
 districtwalk = ["L'Île-Bizard–Sainte-Geneviève",
                  "Verdun",
@@ -29,69 +29,55 @@ districtwalk = ["L'Île-Bizard–Sainte-Geneviève",
 
 disctrictError = ["Lachine", "Rivière-des-Prairies–Pointe-aux-Trembles"]
 
-drone_speed = 45 # km/h
+drone_speed = 50 # km/h
 drive_speed = 10 # km/h
-walk_speed = 15 # km/h
+walk_speed = 5 # km/h
 
 drone_per_district = [
-    ("L'Île-Bizard–Sainte-Geneviève", 2),
-    ("Pierrefonds-Roxboro", 3),
-    ("Saint-Laurent", 5),
-    ("LaSalle", 3),
-    ("Verdun", 1),
-    ("Le Sud-Ouest", 3),
-    ("Côte-des-Neiges–Notre-Dame-de-Grâce",3),
-    ("Outremont",1),
-    ("Ville-Marie",3),
-    ("Le Plateau-Mont-Royal",2),
-    ("Rosemont—La Petite-Patrie",3),
-    ("Villeray-Saint-Michel-Parc-Extension",5),
-    ("Ahuntsic-Cartierville",4),
-    ("Montreal-Nord",2),
-    ("Saint-Leonard",3),
-    ("Mercier–Hochelaga-Maisonneuve",3),
-    ("Anjou",2)
+    ("L'Île-Bizard–Sainte-Geneviève", 4),
+    ("Pierrefonds-Roxboro", 6),
+    ("Saint-Laurent", 6),
+    ("LaSalle", 5),
+    ("Verdun", 2),
+    ("Le Sud-Ouest", 4),
+    ("Côte-des-Neiges–Notre-Dame-de-Grâce",5),
+    ("Outremont",2),
+    ("Ville-Marie",5),
+    ("Le Plateau-Mont-Royal",4),
+    ("Rosemont—La Petite-Patrie",5),
+    ("Villeray-Saint-Michel-Parc-Extension",6),
+    ("Ahuntsic-Cartierville",6),
+    ("Montreal-Nord",4),
+    ("Saint-Leonard",5),
+    ("Mercier–Hochelaga-Maisonneuve",5),
+    ("Anjou",4)
 ]
 
-total_drive = 422
 drive_per_district = [
-    ("L'Île-Bizard–Sainte-Geneviève", 30),
-    ("Pierrefonds-Roxboro", 30),
-    ("Saint-Laurent", 30),
-    ("LaSalle", 20),
+    ("L'Île-Bizard–Sainte-Geneviève", 28),
+    ("Pierrefonds-Roxboro", 28),
+    ("Saint-Laurent", 28),
+    ("LaSalle", 18),
     ("Verdun", 10),
-    ("Le Sud-Ouest", 30),
-    ("Côte-des-Neiges–Notre-Dame-de-Grâce",20),
-    ("Outremont",20),
-    ("Ville-Marie",20),
-    ("Le Plateau-Mont-Royal",20),
-    ("Rosemont—La Petite-Patrie",30),
-    ("Villeray-Saint-Michel-Parc-Extension",30),
-    ("Ahuntsic-Cartierville",30),
-    ("Montreal-Nord",30),
-    ("Saint-Leonard",30),
-    ("Mercier–Hochelaga-Maisonneuve",20),
-    ("Anjou",22)
+    ("Le Sud-Ouest", 28),
+    ("Côte-des-Neiges–Notre-Dame-de-Grâce",18),
+    ("Outremont",18),
+    ("Ville-Marie",18),
+    ("Le Plateau-Mont-Royal",18),
+    ("Rosemont—La Petite-Patrie",28),
+    ("Villeray-Saint-Michel-Parc-Extension",28),
+    ("Ahuntsic-Cartierville",28),
+    ("Montreal-Nord",28),
+    ("Saint-Leonard",28),
+    ("Mercier–Hochelaga-Maisonneuve",18),
+    ("Anjou",18)
 ]
 
 walk_per_district = [
-    ("L'Île-Bizard–Sainte-Geneviève", 2),
-    ("Pierrefonds-Roxboro", 3),
-    ("Saint-Laurent", 5),
-    ("LaSalle", 3),
-    ("Verdun", 1),
-    ("Le Sud-Ouest", 3),
-    ("Côte-des-Neiges–Notre-Dame-de-Grâce",3),
-    ("Outremont",1),
-    ("Ville-Marie",3),
-    ("Le Plateau-Mont-Royal",2),
-    ("Rosemont—La Petite-Patrie",3),
-    ("Villeray-Saint-Michel-Parc-Extension",5),
-    ("Ahuntsic-Cartierville",4),
-    ("Montreal-Nord",2),
-    ("Saint-Leonard",3),
-    ("Mercier–Hochelaga-Maisonneuve",3),
-    ("Anjou",2)
+    ("L'Île-Bizard–Sainte-Geneviève", 20),
+    ("Verdun", 20),
+    ("Outremont",20),
+    ("Montreal-Nord",20)
 ]
 
 drone_routes_colors=['darkcyan',
@@ -211,7 +197,8 @@ def estimate_length(G, path):
     return round(total / 1000, 2)
 
 def estimate_time_drone(distance):
-    return distance / drone_speed
+    t = distance / drone_speed
+    return t * 60
 
 
 def hourstomin(hours):
@@ -266,6 +253,7 @@ def launchDrones():
     print("~~~~~~~~~~~~~   Launching Drones   ~~~~~~~~~~~~~")
     routes = []
     montrealdistricts = []
+    totaldrone = 0
     for dist in mainDistricts:
         printprocess(dist)
         G = get_graph_from(dist)
@@ -273,12 +261,14 @@ def launchDrones():
         path = get_path_from_file(dist)
         lg = estimate_length(G, path)
         drones = getnumberofdrones(dist)
+        totaldrone += drones
         print("     -> Distance travelled : " + str(round(lg/drones, 2)) + " km.")
-        print("     -> Estimated time     : " + str(round(estimate_time_drone(lg) / drones)) + " hour(s).")
-        print("     -> Drones needed : " + str(drones) + " drones.")
+        print("     -> Estimated time     : " + str(round(estimate_time_drone(lg) / drones)) + " minute(s) per drone.")
+        print("     -> Drones needed      : " + str(drones) + " drones.")
         print("")
         routes.append(path)
     montreal = nx.compose_all(montrealdistricts)
+    print("Total : " + str(totaldrone) + " drones.")
     print("SCHEMA DU TRACE DES DRONES DANS LA VILLE (NE MARCHE PAS SUR DOCKER)")
     ox.plot_graph_routes(montreal, routes, route_colors=drone_routes_colors, route_linewidths=2, orig_dest_size=50)
 
@@ -286,6 +276,7 @@ def launchDrive():
     print("~~~~~~~~~~~~~   Launching Road Machines   ~~~~~~~~~~~~~")
     routes = []
     montrealdistricts = []
+    totalmachine = 0
     for dist in mainDistricts:
         printprocess(dist)
         G = get_graph_from(dist)
@@ -293,12 +284,14 @@ def launchDrive():
         path = get_path_from_file(dist)
         lg = estimate_length(G, path)
         machines = getnumberofdrive(dist)
+        totalmachine += machines
         print("     -> Distance travelled : " + str(round(lg/machines, 2)) + " km per machine")
         print("     -> Estimated time     : " + str(round(estimate_time_road(lg/machines))) + " minute(s) per machine.")
-        print("     -> Machines needed : " + str(machines) + " machine(s).")
+        print("     -> Machines needed    : " + str(machines) + " machine(s).")
         print("")
         routes.append(path)
     montreal = nx.compose_all(montrealdistricts)
+    print("Total : " + str(totalmachine) + " machines.")
     print("SCHEMA DU TRACE DES DENEIGEUSES DE ROUTE DANS LA VILLE (NE MARCHE PAS SUR DOCKER)")
     ox.plot_graph_routes(montreal, routes, route_colors=drone_routes_colors, route_linewidths=2, orig_dest_size=50)
 
@@ -306,6 +299,7 @@ def launchWalk():
     print("~~~~~~~~~~~~~   Launching Pedestrian Machines   ~~~~~~~~~~~~~")
     routes = []
     montrealdistricts = []
+    totalmachine = 0
     for dist in mainDistricts:
         G = get_graph_walk_from(dist)
         montrealdistricts.append(G)
@@ -314,12 +308,14 @@ def launchWalk():
             path = get_path_walk_from_file(dist)
             lg = estimate_length(G, path)
             machines = getnumberofwalk(dist)
+            totalmachine += machines
             print("     -> Distance travelled : " + str(round(lg/machines, 2)) + " km per machine")
             print("     -> Estimated time     : " + str(round(estimate_time_road(lg/machines))) + " minute(s) per machine.")
-            print("     -> Machines needed : " + str(machines) + " machine(s).")
+            print("     -> Machines needed    : " + str(machines) + " machine(s).")
             print("")
             routes.append(path)
     montreal = nx.compose_all(montrealdistricts)
+    print("Total : " + str(totalmachine) + " machines.")
     print("SCHEMA DU TRACE DES DENEIGEUSES DE TROTTOIR DANS LA VILLE (NE MARCHE PAS SUR DOCKER)")
     ox.plot_graph_routes(montreal, routes, route_colors=['cyan', 'blue', 'green', 'yellow'], route_linewidths=2, orig_dest_size=50)
 
@@ -334,4 +330,3 @@ def demo():
     launchWalk()
 
 demo()
-
